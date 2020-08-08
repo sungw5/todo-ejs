81dbef7fa333f2023c5b3decef0af330035c64c1
Sung Woo Oh <sungbr95@gmail.com>
2020-08-08 17:19:34 -0400

    css added

--- app.js
+++ app.js
@@ -3,40 +3,40 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = [];
+
+// specify static folder where css,img files are
+app.use(express.static("public"));
+// setting up EJS
 app.set("view engine", "ejs");
+// setting up the body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   let today = new Date();
-  let currentDay = today.getDay();
-  let day = "";
+  //   let currentDay = today.getDay();
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 5:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error current day is out of range");
-  }
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
   //   pass the variable  day
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+/////////////////////////////////////////////////////////////////
+/////////////////////// POST (INPUTS) ///////////////////////////
+/////////////////////////////////////////////////////////////////
+
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+  items.push(item);
+  //  go back to home page
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
